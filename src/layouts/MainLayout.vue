d195287942a49db85482aaaf1a88adcc077f2b9a
Yannick BILCOT <yannick.bilcot@laposte.net>
2021-11-24 10:34:14 +0800

    setup quasar environment

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,24 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-btn flat label="Game Stats" />
+        <q-space />
+        <q-btn color="secondary" label="New" />
+      </q-toolbar>
+    </q-header>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'MainLayout',
+})
+</script>
