706d005998648eaf87344ec1b0bfad2aec600ffe
Yannick BILCOT <yannick.bilcot@laposte.net>
2021-11-26 19:04:55 +0800

    dev: Game page view (in-progress)

--- src/pages/Game.vue
+++ src/pages/Game.vue
@@ -1,6 +1,22 @@
 <template>
   <q-page class="row items-center justify-evenly">
-   </q-page>
+  <div v-if="game" class="column q-pa-md q-gutter-md">
+    <template v-for="{id, name} in game.players" :key="id">
+      <q-card class="text-white bg-secondary"
+        :style="$q.screen.xs ? 'width: 250px;' : 'width: 640px;'"
+      >
+        <q-card-section>
+          <q-item>
+            <q-item-section>{{ name }}</q-item-section>
+            <q-item-section avatar>
+              <q-btn flat round icon="delete" />
+            </q-item-section>
+          </q-item>
+        </q-card-section>
+      </q-card>
+    </template>
+  </div>
+  </q-page>
 </template>
 
 <script lang="ts">
