1ca85f9a1be96dc0001f4dc6558cb726b13ef97e
Yannick BILCOT <yannick.bilcot@laposte.net>
2021-12-03 13:46:13 +0800

    feat: set api path for dev and prod

--- quasar.conf.js
+++ quasar.conf.js
@@ -55,6 +55,11 @@ module.exports = configure(function (ctx) {
     // Full list of options: https://quasar.dev/quasar-cli/quasar-conf-js#Property%3A-build
     build: {
       vueRouterMode: 'history', // available values: 'hash', 'history'
+      env: {
+        API: ctx.dev
+          ? 'http://127.0.0.1:3000/api/v1'
+          : 'api/v1'
+      },
 
       // transpile: false,
       // publicPath: '/',
