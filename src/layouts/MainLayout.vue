a94f6109656dc4087c13f24e2c9866e37eaeb8f1
Yannick BILCOT <yannick.bilcot@laposte.net>
2022-03-03 10:50:24 +0800

    feat: add btn link to swagger doc

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -3,6 +3,8 @@
     <q-header elevated>
       <q-toolbar>
         <q-btn flat label="Game Stats" to="/" />
+        <q-space />
+        <q-btn flat label="API Documentation" type="a" :href="docHref" />
       </q-toolbar>
     </q-header>
 
@@ -18,5 +20,10 @@ import { defineComponent } from 'vue'
 
 export default defineComponent({
   name: 'MainLayout',
+  setup() {
+    return {
+      docHref: process.env.DEV ? 'http://127.0.0.1:3000/swagger/' : '/swagger/'
+    }
+  },
 })
 </script>
